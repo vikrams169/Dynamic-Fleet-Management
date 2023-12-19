139588a50f1320b2e8037902a4e66bd838e507f3
Vikram Setty <vikramsetty169@gmail.com>
2023-12-19 13:35:15 -0500

    tried a different approach to fixing the unit test error. Hopefully should work now

--- src/swarm_control/test/test.cpp
+++ src/swarm_control/test/test.cpp
@@ -88,11 +88,11 @@ TEST_F(EnvironmentTests, test_perform_iteration) {
       testEnvironment->getSimAgentDesiredPositions();
 }
 
-std::vector<double> initial_pos{0, 0};
-std::vector<double> initial_vel{0, 0};
-std::vector<double> new_vel{0, 0};
-std::vector<double> des_vel{0, 0};
-std::vector<double> des_pos{0, 0};
+std::vector<double> initial_pos;
+std::vector<double> initial_vel;
+std::vector<double> new_vel;
+std::vector<double> des_vel;
+std::vector<double> des_pos;
 
 // /**
 //  * @brief Text fixture class for testing the SimAgent class.
